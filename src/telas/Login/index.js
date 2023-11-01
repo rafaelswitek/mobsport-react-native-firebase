b76c3e6b53f8fe8d738b82c07ef91e65d20a327c
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-31 21:04:47 -0300

    03. Autenticação no Login

--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -2,28 +2,61 @@ import React, { useState } from 'react';
 import { View } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
+import { logar } from '../../servicos/requisicoesFirebase';
 import estilos from './estilos';
+import { Alerta } from '../../componentes/Alerta';
 
 export default function Login({ navigation }) {
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [statusError, setStatusError] = useState('');
+  const [mensagemError, setMensagemError] = useState('');
+
+  async function realizarLogin() {
+    if (email == '') {
+      setMensagemError('O email é obrigatório!');
+      setStatusError('email');
+    } else if (senha == '') {
+      setMensagemError('A senha é obrigatória!');
+      setStatusError('senha');
+    } else {
+      const resultado = await logar(email, senha);
+      if (resultado == 'erro') {
+        setStatusError('firebase')
+        setMensagemError('Email ou senha não conferem')
+      }
+      else {
+        navigation.navigate('Principal')
+      }
+    }
+  }
 
   return (
     <View style={estilos.container}>
-      <EntradaTexto 
+      <EntradaTexto
         label="E-mail"
         value={email}
         onChangeText={texto => setEmail(texto)}
+        error={statusError == 'email'}
+        messageError={mensagemError}
       />
       <EntradaTexto
         label="Senha"
         value={senha}
         onChangeText={texto => setSenha(texto)}
         secureTextEntry
+        error={statusError == 'senha'}
+        messageError={mensagemError}
       />
-      
-      <Botao onPress={() => navigation.navigate('Principal')}>LOGAR</Botao>
-      <Botao 
+
+      <Alerta
+        mensagem={mensagemError}
+        error={statusError == 'firebase'}
+        setError={setStatusError}
+      />
+
+      <Botao onPress={() => realizarLogin()}>LOGAR</Botao>
+      <Botao
         onPress={() => { navigation.navigate('Cadastro') }}
       >
         CADASTRAR USUÁRIO
