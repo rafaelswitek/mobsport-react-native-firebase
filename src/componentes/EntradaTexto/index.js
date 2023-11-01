f908d5254a3d23c1323bc5de72b645156434059d
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-31 21:27:17 -0300

    05. Refatorando o projeto

--- src/componentes/EntradaTexto/index.js
+++ src/componentes/EntradaTexto/index.js
@@ -2,15 +2,35 @@ import { useState } from 'react';
 import { TextInput, HelperText } from 'react-native-paper';
 import estilos from './estilos';
 
-export function EntradaTexto({ label, value, onChangeText, secureTextEntry, error, messageError }) {
+export function EntradaTexto({
+  label,
+  value,
+  onChangeText,
+  secureTextEntry,
+  error,
+  messageError,
+  pattern
+}) {
   const [secureMode, setSecureMode] = useState(secureTextEntry);
+
+  function regexValidation() {
+    if (!value) return false;
+    if (pattern) {
+      const condition = new RegExp(pattern);
+      return !condition.test(value);
+    }
+
+    return false;
+  }
+
+  const showError = value == null || error || regexValidation();
 
   return (
     <>
       <TextInput
         label={label}
         value={value}
-        error={error}
+        error={showError}
         secureTextEntry={secureMode}
         onChangeText={onChangeText}
         style={estilos.input}
@@ -18,13 +38,13 @@ export function EntradaTexto({ label, value, onChangeText, secureTextEntry, erro
         activeOutlineColor='#1E8187'
         right={
           secureTextEntry ?
-          <TextInput.Icon
-            name={secureMode ? 'eye-off' : 'eye'}
-            onPress={() => setSecureMode(!secureMode)}
-          /> : null
+            <TextInput.Icon
+              name={secureMode ? 'eye-off' : 'eye'}
+              onPress={() => setSecureMode(!secureMode)}
+            /> : null
         }
       />
-      {error && <HelperText type="error" visible={error}>
+      {showError && <HelperText type="error" visible={showError}>
         {messageError}
       </HelperText>}
     </>
--- src/telas/Login/index.js
+++ src/telas/Login/index.js
@@ -1,67 +1,77 @@
 import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
 import Botao from '../../componentes/Botao';
 import { EntradaTexto } from '../../componentes/EntradaTexto';
 import { logar } from '../../servicos/requisicoesFirebase';
 import estilos from './estilos';
 import { Alerta } from '../../componentes/Alerta';
 import { auth } from '../../config/firebase';
+import { alteraDados, verificaSeTemEntradaVazia } from '../../utils/comum';
+import { entradas } from './entradas';
+
+import animacaoCarregando from '../../../assets/animacaoCarregando.gif';
 
 export default function Login({ navigation }) {
-  const [email, setEmail] = useState('');
-  const [senha, setSenha] = useState('');
+  const [dados, setDados] = useState({
+    email: '',
+    senha: ''
+  })
+
   const [statusError, setStatusError] = useState('');
   const [mensagemError, setMensagemError] = useState('');
+  const [carregando, setCarregando] = useState(true);
 
   useEffect(() => {
     const estadoUsuario = auth.onAuthStateChanged(usuario => {
       if (usuario) {
         navigation.replace('Principal')
       }
+      setCarregando(false)
     })
     return () => estadoUsuario();
   }, [])
 
   async function realizarLogin() {
-    if (email == '') {
-      setMensagemError('O email é obrigatório!');
-      setStatusError('email');
-    } else if (senha == '') {
-      setMensagemError('A senha é obrigatória!');
-      setStatusError('senha');
-    } else {
-      const resultado = await logar(email, senha);
-      if (resultado == 'erro') {
-        setStatusError('firebase')
-        setMensagemError('Email ou senha não conferem')
-      }
-      else {
-        navigation.replace('Principal')
-      }
+    // funcao para verificar se email ou senha sao vazios
+    if (verificaSeTemEntradaVazia(dados, setDados)) return
+
+    const resultado = await logar(dados.email, dados.senha)
+    if (resultado == 'erro') {
+      setStatusError(true)
+      setMensagemError('E-mail ou senha não conferem')
+      return
     }
+    navigation.replace('Principal')
+  }
+
+  if (carregando) {
+    return (
+      <View style={estilos.containerAnimacao}>
+        <Image source={animacaoCarregando}
+          style={estilos.imagem}
+        />
+      </View>
+    )
   }
 
   return (
     <View style={estilos.container}>
-      <EntradaTexto
-        label="E-mail"
-        value={email}
-        onChangeText={texto => setEmail(texto)}
-        error={statusError == 'email'}
-        messageError={mensagemError}
-      />
-      <EntradaTexto
-        label="Senha"
-        value={senha}
-        onChangeText={texto => setSenha(texto)}
-        secureTextEntry
-        error={statusError == 'senha'}
-        messageError={mensagemError}
-      />
+      {
+        entradas.map((entrada) => {
+          return (
+            <EntradaTexto
+              key={entrada.id}
+              {...entrada}
+              value={dados[entrada.name]}
+              onChangeText={valor => alteraDados(entrada.name, valor, dados, setDados)}
+            />
+          )
+        })
+      }
 
       <Alerta
         mensagem={mensagemError}
-        error={statusError == 'firebase'}
+        error={statusError}
         setError={setStatusError}
       />
 
