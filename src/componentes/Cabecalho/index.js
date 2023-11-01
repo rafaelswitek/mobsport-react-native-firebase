38d710b3c6cc353fa3bb9d5a7ec9781d528edb47
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-31 21:16:38 -0300

    04. Persistência do usuário

--- src/componentes/Cabecalho/index.js
+++ src/componentes/Cabecalho/index.js
@@ -2,15 +2,15 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import estilos from './estilos';
 
-export default function Cabecalho({ navigation }) {
+export default function Cabecalho({ logout }) {
 
   return (
     <View style={estilos.container}>
       <Text style={estilos.texto}>Lista de produtos</Text>
-      <TouchableOpacity style={estilos.botao} onPress={() => navigation.replace('Login')}>
-        <Icon 
-          name={'log-out'} 
-          size={20} 
+      <TouchableOpacity style={estilos.botao} onPress={logout}>
+        <Icon
+          name={'log-out'}
+          size={20}
           color="#FFF"
         />
       </TouchableOpacity>
